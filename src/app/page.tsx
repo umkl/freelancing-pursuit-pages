1539c4e87dbf3c16bbfa0a1e802a0c416ce6dada
Michael <michael.ungar03@gmail.com>
2024-07-27 09:45:40 +0200

    adding initial text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,18 @@ export default function Home() {
 	return (
 		<main>
 			<header></header>
-			<section></section>
+			<section>
+				<p>Servus, I bin da</p>
+				<p>Michael Ungar</p>
+				<ul>
+					<li>Web Developer</li>
+					<li>UX Designer</li>
+				</ul>
+				<p>
+					Mit Code & Design versuche ich die digitale Welt sukzessive schöner zu
+					machen.
+				</p>
+			</section>
 		</main>
 	);
 }
