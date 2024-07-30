4280622d4cbc7b605bbc04cbecfd2a1949629bff
Michael <michael.ungar03@gmail.com>
2024-07-30 17:38:36 +0200

    render proper text for / page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,31 @@ import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 export default function Home() {
-	const t = useTranslations("HomePage");
+	const t = useTranslations("/");
+
 	return (
 		<main>
-			<header></header>
+			<header>{t("ad-text-1")}</header>
 			<section>
-				<h1>{t("title")}</h1>
-				<p>Servus, I bin da</p>
+				<p>{t("greeting")}</p>
 				<p>Michael Ungar</p>
 				<ul>
-					<li>Web Developer</li>
-					<li>UX Designer</li>
+					{["webdev", "ux"].map((key) => (
+						<li key={key}>{t("jobs." + key)}</li>
+					))}
 				</ul>
 				<p>
-					Mit Code & Design versuche ich die digitale Welt sukzessive schöner zu
-					machen.
+					{t("mission")
+						.split(/(\[Code\]|\[Design\])/)
+						.map((part, index) => {
+							if (part === "[Code]") {
+								return <span key={index}>Code</span>;
+							} else if (part === "[Design]") {
+								return <span key={index}>Design</span>;
+							} else {
+								return <span key={index}>{part}</span>;
+							}
+						})}
 				</p>
 			</section>
 		</main>
