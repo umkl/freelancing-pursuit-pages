39dbdebeaf0d77f519a9e1a492f0a690d4649f05
Michael <michael.ungar03@gmail.com>
2024-09-28 09:44:25 +0200

    layout adjustments

---
+++ src/app/[lang]/projects/layout.tsx
@@ -0,0 +1,19 @@
+import GobackHeader, { Adheader } from "@/comps/header";
+import { getLocale, unstable_setRequestLocale } from "next-intl/server";
+// import {unstable_setRequestLocale} from 'next-intl/server';
+
+export default async function BaseLayout({
+	children,
+	params,
+}: Readonly<{
+	children: React.ReactNode;
+	params: { lang: string };
+}>) {
+	unstable_setRequestLocale(params.lang);
+	return (
+		<main className="flex-1 relative overflow-hidden flex flex-col laptop:min-h-[700px] bg-red-100">
+			<GobackHeader />
+			{children}
+		</main>
+	);
+}
--- src/app/[lang]/projects/page.tsx
+++ src/app/[lang]/projects/page.tsx
@@ -1,5 +1,19 @@
 const ProjectsPage = ({ params }: { params: { lang: string } }) => {
-	return <div>ok</div>;
+	return (
+		<section className="flex-1 w-full h-full relative overflow-hidden bg-red-200">
+			<div className="h-full w-full overflow-hidden flex flex-col bg-red-300 relative">
+				{/* <div className="flex-1 overflow-y-scroll">
+					{Array.from({ length: 40 }).map((_, index) => {
+						return (
+							<tr key={index} className="border-b-2 border-white h-[2rem]">
+								<td>projectname</td>
+							</tr>
+						);
+					})}
+				</div> */}
+			</div>
+		</section>
+	);
 };
 
 export default ProjectsPage;
