8ed7da233fd60de9308e10d155af1fc291aefa39
Michael <michael.ungar03@gmail.com>
2024-09-30 19:39:12 +0200

    slider

--- src/app/[lang]/(base)/page.tsx
+++ src/app/[lang]/(base)/page.tsx
@@ -8,11 +8,22 @@ import useViewportWidth from "@/utils/hooks/useViewportWidth";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { getLocale, unstable_setRequestLocale } from "next-intl/server";
+import Marquee from "react-fast-marquee";
 const TagMarquee = dynamic(() => import("@/comps/tag-marquee"), { ssr: false });
 // import TagMarquee from "@/comps/tag-marquee";
 
 export default function Home({ params }: { params: { lang: string } }) {
 	unstable_setRequestLocale(params.lang);
+	const featured_projects = [
+		"ballkarten-project-1866x1400",
+		"charge-project-3266x1400",
+		"deets-project-3266x1400",
+		"filliStudio-project-3266x1400",
+		"heed-project-3266x1400",
+		"octagon-project-1866x1400",
+		"synopsis-project-2100x1400",
+		"wbyt_web-project-1866x1400",
+	];
 	const t = useTranslations(Routes.root);
 	return (
 		<section className="mx-4 tablet:mx-8 tablet:mt-4 box-border grow h-full flex flex-col relative">
@@ -71,16 +82,29 @@ export default function Home({ params }: { params: { lang: string } }) {
 						</li>
 					))}
 				</ul>
-				<div className="laptop:absolute w-full laptop:w-[55%] h-full pt-5 custombp:max-h-[500px] bottom-0 right-0 z-0">
-					<Image
-						className="relative h-full w-full object-cover rounded-lg"
-						src={"/imgs/gabriel-garcia-marengo-qJvpykJ5SKs-unsplash.jpg"}
-						alt={"Image from Gabriel Garcia Marengo of Mountains."}
-						width={"3000"}
-						height={"1000"}
-					/>
+				<div className="laptop:absolute w-full laptop:w-[55%] h-fit mt-5 pt-5 custombp:max-h-[500px] bottom-0 right-0 z-0 overflow-hidden bg-neutral-200 rounded-lg">
+					<Marquee speed={300} pauseOnHover>
+						<div className="flex flex-nowrap mb-4 gap-4 mr-10">
+							{featured_projects.map((x) => {
+								return (
+									<Image
+										src={`/imgs/project-imgs/${x}.png`}
+										style={{
+											width: "auto",
+											height: "400px",
+										}}
+										width={600}
+										height={400}
+										quality={100}
+										objectFit="contain"
+										alt={`/images/project-imgs/${x}`}
+									/>
+								);
+							})}
+						</div>
+					</Marquee>
 					<div className="absolute font-black uppercase font-clash text-lg m-4 mr-8 z-10 mix-blend-difference right-0 bottom-0">
-						austria
+						@UMKL
 					</div>
 				</div>
 			</div>
